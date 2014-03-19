1ef6f4e6ce0b6c2057d8b7306c8edabffa0da2e7
Miriam Ashton <miriam_ashton@brown.edu>
2014-03-18 21:09:18 -0400

    added testing getallstreetnodes method to gui backend

--- src/cs32/maps/FileReader/MapsIO.java
+++ src/cs32/maps/FileReader/MapsIO.java
@@ -414,6 +414,33 @@ public class MapsIO {
 	}
 
 
+	/**
+	 * Given a wayID, search the *ways file* for correct line
+	 * and return a Way object with all relevant info
+	 */
+	public List<Way> getAllWays() throws IOException {
+		List<Way> points = new ArrayList<>();
+		
+		RandomAccessFile raf = new RandomAccessFile(waysFile, "r");
+		nextNewLine(raf); //skip header with titles
+		String inp = "";
+		long fileSize = raf.length();
+		while(raf.getFilePointer() < fileSize) {
+			inp = this.readOneLine(raf);
+			String[] line = inp.split("\t");
+			String id = line[ways_idCol];
+			String startID = line[ways_startCol];
+			String endID = line[ways_endCol];
+			String name = line[ways_nameCol];
+			points.add(new Way(id, startID, endID, name));
+			nextNewLine(raf);
+		}
+		raf.close();
+
+		return points;
+	}
+	
+	
 	/**Get page: Return a list of location nodes that have the same first 4 digits
 	 * of latitude and first 4 digits of longitude @qttran
 	 * @param node
